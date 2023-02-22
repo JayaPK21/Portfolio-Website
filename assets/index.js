7dd729001eef8e84cf6455979625ab3221d52f49
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-02-22 09:33:08 +0000

    Adds code to fix the position of footer

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,23 @@
 $('#hero-banner').click(function() {
     $('#attributionFooter').addClass('d-none');
     $('#hero-banner').slideUp("slow", function() {
+
         $('#mainContainer').removeClass('d-none');
-        //$('#hero-banner').addClass('d-none');
+        $('#mainFooter').removeClass('d-none');
         $('#hero-banner').removeClass('d-flex');
+
     });
 });
 
 $('#nav-contact-tab').click(function() {
+    $('#mainFooter').addClass('d-none');
     $('#hero-banner').slideDown("slow", function() {
-        // $('#mainContainer').removeClass('d-none');
-        // $('#hero-banner').removeClass('d-none');
+
+        $('#mainContainer').addClass('d-none');
+        
+        
         $('#hero-banner').addClass('d-flex');
         $('#attributionFooter').removeClass('d-none');
+
     });
 })
